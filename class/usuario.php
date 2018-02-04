21866b14f62a04494bc1df4dab3cc6418516d68d
Exspiravitt <fernandojcsouza@yahoo.com.br>
2018-02-04 16:19:12 -0200

    metodos de lista

--- class/usuario.php
+++ class/usuario.php
@@ -75,10 +75,59 @@ class Usuario{
 		}
 	}
 
+// getList incluído na aula 64, falando sobre listagem de dados de um bd
+	public static function getList(){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+
+	}
+
+	public static function search($login){
+
+		$sql = new sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+
+				':SEARCH'=>"%".$login."%"
+
+			));
+
+	}
+
+	public function login($login, $password){
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(":LOGIN"=>$login, ":PASSWORD"=>$password));
+
+		if (count($results) > 0){
+
+			$row = $results[0];
+
+			$this->setIdusuario($row['idusuario']);
+
+			$this->setDeslogin($row['deslogin']);
+
+			$this->setDessenha($row['dessenha']);
+
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+		}
+
+		else{
+
+			throw new Exception("Login e/ou senha inválidos!", 1);
+
+		}
+
+	}
+
 	public function __toString(){
 
 		return json_encode(array(
-			
+
 			"idusuario"=>$this->getIdusuario(),
 
 			"deslogin"=>$this->getDeslogin(),
--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once("config.php");
 /*
 ##############################################################################################
 
-Programação feita na aula 62 e comentada na aula 63, para incluir um novo modo de programação.
+// Programação feita na aula 62 e comentada na aula 63, para incluir um novo modo de programação.
 
 $sql = new Sql();
 
@@ -16,10 +16,50 @@ echo json_encode($usuarios);
 ##############################################################################################
 */
 
+/*
+##############################################################################################
+
+// Carrega um usuário (aula 63), comentado na aula 64.
+
 $root = new Usuario();
 
 $root->loadById(4);
 
 echo $root;
+
+##############################################################################################
+*/
+
+/*
+##############################################################################################
+
+// Carrega uma lista de usuários. Feito na aula 64 e comentado nesta mesma aula.
+
+$lista = Usuario::getList();
+
+echo json_encode($lista);
+
+##############################################################################################
+*/
+
+/*
+##############################################################################################
+
+// Carrega uma lista de usuários buscando pelo login. Feito na aula 64 e comentado na mesma aula
+
+$search = Usuario::search("jo");
+
+echo json_encode($search);
+
+##############################################################################################
+*/
+
+// Carrega um usuário usando o login e a senha. Feito na aula 64.
+
+$usuario = new Usuario();
+
+$usuario->login("user", "123");
+
+echo $usuario;
 
 ?>
